bb8c5025c005807913efb0327589a44d1edd6fc1
Hernan Garcia <hernan@dynamicprogrammer.com>
2012-07-19 00:13:06 -0400

    Adding a parameter of Options type to the Optional constructor instead of extend RuntimeOptions.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Optional
 ========
+
+A package to parse arguments to use on Dart console applications.
+
+## Usage
+
--- optional.dart
+++ optional.dart
@@ -1,13 +1,15 @@
 #library("optional");
 #import("dart:coreimpl");
 
-class Optional extends RuntimeOptions {
+class Optional {
+  Options _options;
+  Optional(this._options);
   
-  List get(String argument) {
+  List get(String key) {
     var next = false;
-    var arg = "-${argument}";
-    if (argument.length > 1) {
-      arg = "-${arg}";
+    var arg = "-$key";
+    if (key.length > 1) {
+      arg = "-$arg";
     }
     
     finder(ele) {
@@ -21,7 +23,7 @@ class Optional extends RuntimeOptions {
       return false;
     }
     
-    var options = arguments.filter(finder);
+    var options = _options.arguments.filter(finder);
     return options;
   }
 }
